6ef4748ad05ec6dc9a8bc4447c13b9a52872652c
Ranga_Kidambi <40720902+rkidambi11@users.noreply.github.com>
2019-05-27 15:16:23 +0530

    edit keypath

--- ec2.tf
+++ ec2.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "sg_22" {
 
 resource "aws_key_pair" "mykey" {
   key_name = "mykey"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+  public_key = "${file("${var.public_key_path}")}"
 }
 resource "aws_instance" "testInstance" {
   ami           = "${var.instance_ami}"
